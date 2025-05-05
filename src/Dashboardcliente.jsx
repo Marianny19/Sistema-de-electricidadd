5f87ff306ba94492170cff56e0ac594099692cc0
anabellfabian <anabellfabian29@gmail.com>
2025-05-05 12:25:07 -0400

    Creacion de citas y notas del cliente

---
+++ src/Dashboardcliente.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCalendar, faCartArrowDown, faChevronLeft, faClipboard,
+  faFileInvoice, faFileInvoiceDollar, faHome, faMoneyCheck,
+  faReceipt,
+  faSignOut, faUser, faUsers
+} from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+
+const MiComponente = () => (
+  <div>
+    <FontAwesomeIcon icon={faHome} />
+    Inicio
+  </div>
+);
+const Dashboardcliente = () => {
+  const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarCollapsed(!sidebarCollapsed);
+  };
+
+  const cerrarSesion = () => {
+    console.log("Cerrar sesión");
+  };
+
+  return (
+    <div className="dashboard">
+      <div className={`sidebar ${sidebarCollapsed ? "collapsed" : ""}`}>
+        <h2>Bienvenido</h2>
+        <ul>
+          <li><a href="/dashboardcliente"><FontAwesomeIcon icon={faHome} /> <span>Inicio</span></a></li>
+          <li><Link to="/clienteregistro"><FontAwesomeIcon icon={faUsers} /> <span>Cliente</span></Link></li>
+          <li><Link to="/citaregistro"><FontAwesomeIcon icon={faCalendar} /> <span>Citas</span></Link></li>
+          <li><a href=""><FontAwesomeIcon icon={faFileInvoice} /> <span>Cotización</span></a></li>
+          <li><Link to=""><FontAwesomeIcon icon={faReceipt} /> <span>Recomendación</span></Link></li>
+          <li><a href="/notasregistro"> <FontAwesomeIcon icon={faClipboard} /><span>Notas</span></a></li>
+        </ul>
+        <ul>
+          <li className="Cerrarsesion"><a href="#" onClick={cerrarSesion}><FontAwesomeIcon icon={faSignOut} /><span>Cerrar sesión</span></a></li>
+        </ul>
+        <button className="toggle-btn" onClick={toggleSidebar}><FontAwesomeIcon icon={faChevronLeft} /></button>
+      </div>
+
+      <div className="dashboard-content">
+        <h2>Bienvenido a Servicios Multiples Pérez</h2>
+        <div className="widgets">
+          <div className="widget cita">
+            <h3>MISIÓN</h3>
+            <div id="lista-citas">
+              <p>Brindar soluciones integrales en electricidad residencial, comercial e industrial con altos estándares de c
+                alidad, seguridad y responsabilidad, satisfaciendo las necesidades de nuestros clientes mediante un servicio
+                 eficiente, innovador y confiable.</p>
+            </div>
+          </div>
+
+          <div className="widget">
+            <h3>VISIÓN</h3>
+            <div id="lista-servicios">
+              <p>Ser la empresa destacada en servicios eléctricos, reconocida por su compromiso con la excelencia, la innovación 
+                tecnológica y la satisfacción del cliente, contribuyendo al desarrollo sostenible de la comunidad.</p>
+            </div>
+          </div>
+
+          <div className="widget">
+            <h3>VALORES</h3>
+            <div id="lista-servicios-atrasados">
+              <p>Responsabilidad, honestidad, compromiso, calidad, innovacion y trabajo en equipo.</p>
+            </div>
+          </div>
+
+          <div className="widget">
+            <h3>¿QUIÉNES SOMOS?</h3>
+            <p>En Servicios Múltiples Pérez somos una empresa dedicada a ofrecer soluciones eléctricas confiables, seguras y 
+              eficientes para hogares, negocios e industrias. Contamos con un equipo de técnicos capacitados y comprometidos
+               con brindar un servicio de alta calidad, cumpliendo con las normas eléctricas vigentes y adaptándonos a las 
+               necesidades de cada cliente. </p>
+               
+              <p>Desde nuestros inicios, nos hemos enfocado en construir relaciones de confianza, ofreciendo atención personalizada,
+                asesoría experta y respuestas rápidas ante cualquier requerimiento eléctrico. Nuestro objetivo es garantizar la 
+                tranquilidad y satisfacción de quienes confían en nosotros, siendo su aliado en cada proyecto, mantenimiento o 
+                emergencia eléctrica.</p>
+          </div> 
+        </div>
+        <br />
+        <div className="widget">
+            <h3>NUESTROS SERVICIOS</h3>
+            <p>
+              <ul>Instalaciones eléctricas residenciales, comerciales e industriales</ul>
+              <ul>Reparaciones eléctricas</ul>
+              <ul>Instalación y programación de temporizadores y sistemas automatizados</ul>
+              <ul>Instalación de lámparas e inversores</ul>
+              <ul>Mantenimiento eléctrico</ul>
+              <ul>Servicios de plomeria</ul>
+              <ul>Instalacion y mantenimiento de aires/ac</ul>
+            </p>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboardcliente;
--- src/main.jsx
+++ src/main.jsx
@@ -12,21 +12,30 @@ import Crearcita from './Crearcita.jsx';
 import CrearCliente from './CrearCliente.jsx';
 import Factura from './Factura.jsx';
 import Facturaform from './Facturaform.jsx'; 
+import Dashboardcliente from './Dashboardcliente.jsx';
+import Clienteregistro from './Clienteregistro.jsx';
+import Citaregistro from './Citaregistro.jsx';
+import Notasregistro from './Notasregistro.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
         {}
+        <Route path="/" element={<Identificacion />} />
+        <Route path="/dashboardcliente" element={<Dashboardcliente />} /> {}
         <Route path="/dashboard" element={<Dashboard />} /> {}
         <Route path='/clienteempleado' element={<Clienteempleado />} /> {}
         <Route path='/empleado' element={<Empleado />} /> {}
         <Route path="/Iniciosesion" element={<Iniciosesion />} /> {}
-        <Route path='/formulariocita' element={<FormularioCita />} /> {/* Ruta para el formulario de citas */}
-        <Route path='/crearcita' element={<Crearcita />} /> {/* Ruta para crear una cita */}
-        <Route path='/crearcliente' element={<CrearCliente />} /> {/* Ruta para crear un cliente */}
-        <Route path='/factura' element={<Factura />} /> {/* Ruta para la factura */}
-        <Route path='/facturaform' element={<Facturaform />} /> {/* Ruta para el formulario de factura */}
+        <Route path='/formulariocita' element={<FormularioCita />} /> {}
+        <Route path='/crearcita' element={<Crearcita />} /> {}
+        <Route path='/crearcliente' element={<CrearCliente />} /> {}
+        <Route path='/factura' element={<Factura />} /> {}
+        <Route path='/facturaform' element={<Facturaform />} /> {}
+        <Route path='/clienteregistro' element={<Clienteregistro />} /> {}
+        <Route path='/citaregistro' element={<Citaregistro />} /> {}
+        <Route path='/notasregistro' element={<Notasregistro />} /> {}
       </Routes>
     </Router>
   </StrictMode>
