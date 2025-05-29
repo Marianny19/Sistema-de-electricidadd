e1514113deed44991b11137969f58f72fdda76ba
Marianny19 <145990122+Marianny19@users.noreply.github.com>
2025-05-29 08:10:04 -0400

    correcion

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,6 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { RutaProtegida, RutaPublicaSoloParaNoLogueados, RutaProtegidaPorRol } from './RutaProtegida.jsx';
 import './index.css';
