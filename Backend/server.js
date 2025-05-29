b626f0d5c37d30a0f285b3706e50394bcbead23b
Marianny19 <145990122+Marianny19@users.noreply.github.com>
2025-05-29 10:58:23 -0400

    puerto

--- Backend/server.js
+++ Backend/server.js
@@ -110,7 +110,7 @@ Pago.belongsTo(Factura, { foreignKey: 'factura_id' });
 
 
 const app = express();
-const port = 8081;
+const port = process.env.PORT || 8081;
 
 app.use(cors()); 
 app.use(express.json());
