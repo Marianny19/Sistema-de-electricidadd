aaa59a1f88877aae9b3a43c4ae783741336a3ca8
FranklinJFrame <marianny121902@gmail.com>
2025-05-31 17:39:48 -0400

    Update Iniciosesion.jsx

--- src/Iniciosesion.jsx
+++ src/Iniciosesion.jsx
@@ -23,7 +23,7 @@ const Iniciosesion = () => {
     }
 
     try {
-      const response = await fetch('https://sistema-de-electricidadd-copy-production.up.railway.app/login', {
+      const response = await fetch('https://backend-qgtf.onrender.com/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(credenciales)
