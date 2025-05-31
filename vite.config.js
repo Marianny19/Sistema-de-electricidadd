6dfa371f85ec960f0829da145c4317ee539a2e27
FranklinJFrame <marianny121902@gmail.com>
2025-05-31 18:23:45 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: process.env.PORT || 5173,
-    host: '0.0.0.0',
+    host: '0.0.0.0', // You can keep this as '0.0.0.0' if you're deploying to a remote server
+    allowedHosts: [
+      'sistema-de-electricidadd-production-64cd.up.railway.app', // Add the host here
+      'localhost', // You can also add 'localhost' for local development
+    ]
   },
   build: {
     outDir: 'dist',
