23d30dcc7398ad80dac56bc8221d80d79847293f
anabellfabian <anabellfabian29@gmail.com>
2025-05-31 22:48:10 -0400

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -117,7 +117,8 @@ const allowedOrigins = ['sistema-de-electricidadd-production-f62b.up.railway.app
 //se supone que eso cambia no, porque ahora es a railway que la estas subiendo?
 
 app.use(cors({
-  origin: allowedOrigins
+  origin: allowedOrigins,
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));
 
 app.use(express.json());
